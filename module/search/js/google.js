58105be889cc6667324af981d30440e120a32cc7
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-18 05:45:16 +0000

    - remove the search module.

--- module/search/js/google.js
+++
@@ -1,5 +0,0 @@
-    var googleSearchIframeName = "cse-search-results";
-    var googleSearchFormName = "cse-search-box";
-    var googleSearchFrameWidth = 960;
-    var googleSearchDomain = "www.google.com";
-    var googleSearchPath = "/cse";
--- module/search/view/google.html.php
+++
@@ -1,8 +0,0 @@
-<?php include '../../common/view/header.html.php'; ?>
-<div class='row'>
-  <div class='u-1'>
-    <div id="cse-search-results"></div>
-    <script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
-  </div>
-</div>
-<?php include '../../common/view/footer.html.php'; ?>
