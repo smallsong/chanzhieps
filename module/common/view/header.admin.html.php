a6ff1ad68b739ca83c6533e9a12ba24c5d64f0ce
daitingting <daitingting@xirangit.com>
2013-06-18 01:09:49 +0000

    * modify header of admin

--- module/common/view/header.admin.html.php
+++ module/common/view/header.admin.html.php
@@ -1,32 +1,31 @@
 <?php include 'header.lite.html.php';?>
-  <div class="navbar navbar-fixed-top">
-    <div class="navbar-inner">
-      <div class="navbar-container">
-         <a href="/" class="brand"><img src="<?php echo $defaultTheme . '/images/default/xirang_top_logo.png';?>" alt="xirang" /></a>
-         <div class="nav-collapse collapse">
-           <ul class="nav">
-             <li class="active"><a href="#">Home</a></li>
-             <li><a href="#about">About</a></li>
-             <li><a href="#contact">Contact</a></li>
-             <li class="dropdown">
-               <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
-               <ul class="dropdown-menu">
-                 <li><a href="#">Action</a></li>
-                 <li><a href="#">Another action</a></li>
-                 <li><a href="#">Something else here</a></li>
-                 <li class="divider"></li>
-                 <li class="nav-header">Nav header</li>
-                 <li><a href="#">Separated link</a></li>
-                 <li><a href="#">One more separated link</a></li>
-               </ul>
-             </li>
-           </ul>
-           <ul class="nav pull-right">	
-	          <li><a href="#/setting">设置</a></li>		
-              <li class="divider-vertical"></li>								
-              <li><a href="#/logout">退出</a></li>		
-     	   </ul>
-         </div><!--/.nav-collapse -->
-       </div><!--/.container -->
-     </div><!--/.navbar-inner -->    
-   </div>
+<div class="navbar">
+  <div class="navbar-inner">
+    <div class="navbar-container">
+      <a href="/" class="brand"><img src="<?php echo $defaultTheme . '/images/default/xirang_top_logo.png';?>" alt="xirang" /></a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+            <li class="active"><a href="/admin.php">首页</a></li>
+            <li><?php echo html::a($this->createLink('article', 'browseAdmin'), $lang->admin->manageArticle, 'mainwin');?></li>
+            <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="nav-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+        </ul>
+        <ul class="nav pull-right">	
+          <li><?php echo html::a($this->createLink('site' , 'set') , $lang->admin->setSite);?></li>		
+          <li class="divider-vertical"></li>								
+          <li><a href="#/logout">退出</a></li>		
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div><!--/.container -->
+  </div><!--/.navbar-inner -->    
+</div>
