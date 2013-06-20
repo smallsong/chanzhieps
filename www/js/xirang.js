5e833e1b906fc214f9369f532f96ebfc822e6bb7
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-20 05:22:20 +0000

    * adjust code format.  * change xview to v.

--- www/js/xirang.js
+++ www/js/xirang.js
@@ -3,110 +3,116 @@
  *
  * Version 2.1.1
  */
-$.extend({
-        ajaxForm: function(formID, params, targetID)
-        {
-             form = $(formID); 
-             //自定义ajax 出错提示信息
-             var errorFun = function(a, b, c)
-             {
-                 targetID && $(targetID).html('<center style="color:red"><b>'+xview.lang.failed+'</b></center>'+a.responseText);       
-                 $.enableForm(formID);
-             };
+$.extend(
+{
+    ajaxForm: function(formID, params, targetID)
+    {
+         form = $(formID); 
+         //自定义ajax 出错提示信息
+         var errorFun = function(a, b, c)
+         {
+            targetID && $(targetID).html('<center style="color:red"><b>'+v.lang.failed+'</b></center>'+a.responseText);       
+            $.enableForm(formID);
+         };
 
-             var options = 
+         var options = 
+         {
+             target : (params.dataType == null ) ? targetID : null,
+             timeout : params.timeout || 30000,
+             dataType: params.dataType || null,
+
+             beforeSubmit: function()
              {
-                  target : (params.dataType == null ) ? targetID : null,
-                  beforeSubmit: function()
-                  {
-                       if($.isFunction(params.beforeSubmit) && false===params.beforeSubmit()) return false;
-                       $.disableForm(formID);
-                       return true;
-                  },
-                  timeout : params.timeout || 30000,
-                  dataType: params.dataType || null,
-                  success:function(data)
-                  {
-                    if(data == null)
-                    {
-                        alert(xview.lang.noResponse);
-                        $.enableForm(formID);
-                        return false; 
-                    }
-                      $.isFunction(params.success && params.success(data));
-                      if(data.locateTo != "") location.href = data.locateTo;
-                      $.enableForm(formID);
-                  },
-                  beforeSend:function(xml)
-                  {
-                      try
-                      {
-                          xml.setRequestHeader("x", (h.attr('name')||'')+"/"+hs.size()+"/"+(h.val()||'')+"/"+window.screen.height+"/"+window.screen.width);
-                      }
-                      catch(e)
-                      {                        
-                      }
-                  },
-                  error:errorFun
-             };
-             if(true==params.validate)
+                 if($.isFunction(params.beforeSubmit) && false===params.beforeSubmit()) return false;
+                 $.disableForm(formID);
+                 return true;
+             },
+
+             success:function(data)
              {
-                 form.addClass('validateForm');
-                 var validateOption = {submitHandler: function() {form.ajaxSubmit(options);}};
-                 params.validateOption  && $.extend(validateOption, params.validateOption);
-                 params.validateRule && $.extend(validateOption, {rules:params.validateRule});
-                 if(params.errorToTip)
+                 if(data == null)
                  {
-                   var validateErrorPlace = function(error, element)
-                   {
+                     alert(v.lang.noResponse);
+                     $.enableForm(formID);
+                     return false; 
+                 }
+                 $.isFunction(params.success && params.success(data));
+                 if(data.locateTo != "") location.href = data.locateTo;
+                 $.enableForm(formID);
+             },
+
+             beforeSend:function(xml)
+             {
+                 try
+                 {
+                     xml.setRequestHeader("x", (h.attr('name')||'')+"/"+hs.size()+"/"+(h.val()||'')+"/"+window.screen.height+"/"+window.screen.width);
+                 }
+                 catch(e)
+                 {            
+                 }
+             },
+             error:errorFun
+         };
+
+         if(true==params.validate)
+         {
+             form.addClass('validateForm');
+             var validateOption = {submitHandler: function() {form.ajaxSubmit(options);}};
+             params.validateOption  && $.extend(validateOption, params.validateOption);
+             params.validateRule && $.extend(validateOption, {rules:params.validateRule});
+
+             if(params.errorToTip)
+             {
+                 var validateErrorPlace = function(error, element)
+                 {
                      var nextTip=element.next('span.tip');
                      var tip=element.parents("td").find('span.tip');
                      var tipsize=tip.size();
                      if(nextTip.size())
                      {
-                       error.appendTo(nextTip);
+                         error.appendTo(nextTip);
                      }
                      else if(tipsize)
                      {
-                       error.appendTo(tip.eq(tipsize-1));
+                         error.appendTo(tip.eq(tipsize-1));
                      }
                      else
                      {
-                       error.insertAfter(element);
+                         error.insertAfter(element);
                      }
                     };//end validateErrorPlace
-                     var fun= (params.validateErrorPlace && $.isFunction(params.validateErrorPlace))?params.validateErrorPlace:validateErrorPlace;
-                     $.extend(validateOption, {errorPlacement: fun});
-                  }
-                form.validate(validateOption);
-             }
-             else
-             {
-               form.submit(function(){ $(this).ajaxSubmit(options);return false; });
-             }
-            
-        },
-        disableForm:function(formID)
-        {
-          $(formID).find(':submit').attr('disabled', true);
-        },    
-    
 
-        enableForm:function(formID)
-        {
-          $(formID).find(':submit').attr('disabled', false);
-        },    
-    
-        trSelect:function(tableSelector)
-        {
-            var table=$(tableSelector||'.trSelect'),
-                fn=function(){
-                  table.find('tr').removeClass('tr_select');
-                  $(this).parents("tr").addClass("tr_select").find('*').attr('bgcolor','');
-              };
-            table.find('tbody td *').bind('click',fn);
-            table.find('tbody td').bind('dblclick',fn);
-        },
-  
-  });
+                    var fun= (params.validateErrorPlace && $.isFunction(params.validateErrorPlace))?params.validateErrorPlace:validateErrorPlace;
+                    $.extend(validateOption, {errorPlacement: fun});
+              }
 
+         form.validate(validateOption);
+         }
+         else
+         {
+             form.submit(function(){ $(this).ajaxSubmit(options);return false; });
+         }
+        
+    },
+    disableForm:function(formID)
+    {
+        $(formID).find(':submit').attr('disabled', true);
+    },    
+    
+    enableForm:function(formID)
+    {
+        $(formID).find(':submit').attr('disabled', false);
+    },    
+    
+    trSelect:function(tableSelector)
+    {
+        var table=$(tableSelector||'.trSelect'),
+        fn=function()
+        {
+            table.find('tr').removeClass('tr_select');
+            $(this).parents("tr").addClass("tr_select").find('*').attr('bgcolor','');
+        };
+        table.find('tbody td *').bind('click',fn);
+        table.find('tbody td').bind('dblclick',fn);
+    },
+});
