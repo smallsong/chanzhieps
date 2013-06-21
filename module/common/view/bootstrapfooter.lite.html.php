f28dff4f96063a0e9497afc8285b6999e77bbd1b
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-21 07:58:24 +0000

    - remove them.

--- module/common/view/bootstrapfooter.lite.html.php
+++
@@ -1,4 +0,0 @@
-    <?php js::import($jsRoot . 'jquery/bootstrap/bootstrap.min.js', $config->version);?>
-    <div class='hidden'><iframe name='hiddenwin' id='hiddenwin' class='<?php $config->debug ? print('debugwin') : print('hidden');?>'></iframe></div>
-  </body>
-</html>
