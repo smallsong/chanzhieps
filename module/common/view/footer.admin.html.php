83056db31bd3a3f5d6b56b001cb462847bb9d4c4
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-26 00:50:07 +0000

    * move the ajaxform js code to admin footer.

--- module/common/view/footer.admin.html.php
+++ module/common/view/footer.admin.html.php
@@ -1,3 +1,10 @@
-<?php if(isset($pageJS)) js::execute($pageJS);?>
+<?php
+if($config->debug)
+{
+    js::import($jsRoot . 'jquery/form/min.js');
+    js::import($jsRoot . 'jquery/form/xirang.js');
+}
+if(isset($pageJS)) js::execute($pageJS);
+?>
 </body>
 </html>
--- module/user/view/login.admin.html.php
+++ module/user/view/login.admin.html.php
@@ -1,11 +1,12 @@
 <?php include '../../common/view/header.lite.html.php';?>
 <div class="container">
   <form method="post" id='ajaxForm' class="radius-5px">
-    <div id='logo'><?php echo html::image("$defaultTheme/images/main/logo.login.png");?></div>
+    <div id='logo'><?php echo html::image("$themeRoot/default/images/main/logo.login.png");?></div>
     <?php echo html::input('account','','class="input-block-level"');?>
     <?php echo html::password('password','','class="input-block-level"');?>
     <?php echo html::checkbox('keepLogin', $lang->user->login->keepLogin);?>
     <?php echo html::submitButton($lang->user->login->common, 'btn btn-primary');?>
+    <span id='responser'></span>
   </form>
 </div>
 <?php include '../../common/view/footer.admin.html.php';?>
