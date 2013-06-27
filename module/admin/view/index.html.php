971e6b7360036c5ff8ebd444c260e06a6c988026
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-27 07:09:41 +0000

    * rewrite the index page.

--- module/admin/view/index.html.php
+++ module/admin/view/index.html.php
@@ -10,32 +10,22 @@
  */
 ?>
 <?php include '../../common/view/header.admin.html.php';?>
-<?php include '../../common/view/colorbox.html.php';?>
-<div class='row mt-10px'>
-  <div class='u-1'>
-    <table class='table-4' align='center'>
-      <caption><?php echo $lang->admin->selectSite;?></caption>
-      <tr>
-        <td>
-          <?php
-          foreach($sites as $siteID => $siteName)
-          {
-              echo "<p><span class='f-16px strong'>" . html::a(inlink('site', "id=$siteID"), $siteName) . '</span>(';
-              echo html::a($this->createLink('site', 'edit', "id=$siteID"), $lang->edit, '', 'class="colorbox"');
-              if($app->user->isSuper) echo html::a($this->createLink('site', 'delete', "id=$siteID"), $lang->delete, 'hiddenwin');
-              echo ')</p>';
-          }
-          ?>
-        </td>
-        <td class='a-center'>
-          <?php 
-          echo '<p>' . html::a($this->createLink('site', 'create'), $lang->admin->addSite, '', "class='colorbox f-16px strong'") . '</p>';
-          echo '<p>' . html::a($this->createLink('admin', 'navigate'), $lang->admin->navigate, '', "class='f-16px strong'") . '</p>';
-          ?>
-        </td>
-      </tr>
-    </table>
-    <div class='a-center f-16px'><?php echo html::a($this->createLink('user', 'logout'), $lang->logout);?></div>   
+<div class="hero-unit">
+  <h1></h1>
+  <p></p>
+</div>
+<div class="row-fluid">
+  <div class="span4">
+    <h2>article</h2>
+    <p></p>
+  </div>
+  <div class="span4">
+    <h2>category</h2>
+    <p></p>
+  </div>
+  <div class="span4">
+    <h2>site</h2>
+    <p></p>
   </div>
 </div>
 <?php include '../../common/view/footer.admin.html.php';?>
