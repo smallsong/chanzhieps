5b61b1dd833795c7051090d51193fca9604ee1ce
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-27 07:05:40 +0000

    * rewrite the header and footer for admin.

--- module/common/view/footer.admin.html.php
+++ module/common/view/footer.admin.html.php
@@ -1,9 +1,18 @@
+  <?php if($moduleMenu) echo '</div>';?>
+  </div>    
+</div>
+
+<div class="navbar navbar-fixed-bottom">
+  <div class='navbar-inner'>
+    <div class='container-fluid'>
+      <div class='navbar-text pull-right'><?php printf($lang->poweredBy, $config->version);?></div>
+    </div>
+  </div>
+</div>
+
 <?php
-if($config->debug)
-{
-    js::import($jsRoot . 'jquery/form/min.js');
-    js::import($jsRoot . 'jquery/form/xirang.js');
-}
+if($config->debug) js::import($jsRoot . 'jquery/form/min.js');
+if($config->debug) js::import($jsRoot . 'jquery/form/xirang.js');
 if(isset($pageJS)) js::execute($pageJS);
 ?>
 </body>
--- module/common/view/header.admin.html.php
+++ module/common/view/header.admin.html.php
@@ -1,31 +1,17 @@
 <?php include 'header.lite.html.php';?>
-<div class="navbar">
+<div class="navbar navbar-inverse navbar-fixed-top">
   <div class="navbar-inner">
-    <div class="navbar-container">
-      <a href="/" class="brand"><img src="<?php echo $defaultTheme . '/images/default/xirang_top_logo.png';?>" alt="xirang" /></a>
-      <div class="nav-collapse collapse">
-        <ul class="nav">
-            <li class="active"><a href="/admin.php">首页</a></li>
-            <li><?php echo html::a($this->createLink('article', 'browseAdmin'), $lang->admin->manageArticle, 'mainwin');?></li>
-            <li class="dropdown">
-            <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-                <li class="divider"></li>
-                <li class="nav-header">Nav header</li>
-                <li><a href="#">Separated link</a></li>
-                <li><a href="#">One more separated link</a></li>
-              </ul>
-            </li>
-        </ul>
-        <ul class="nav pull-right">	
-          <li><?php echo html::a($this->createLink('site' , 'set') , $lang->admin->setSite);?></li>		
-          <li class="divider-vertical"></li>								
-          <li><a href="#/logout">退出</a></li>		
-        </ul>
-      </div><!--/.nav-collapse -->
-    </div><!--/.container -->
-  </div><!--/.navbar-inner -->    
+    <div class="container-fluid">
+      <?php echo html::a($this->createLink($this->config->default->module), $lang->xirangEPS, '', "class='brand'");?>
+      <?php echo commonModel::createMainMenu($this->moduleName);?>
+      <?php echo commonModel::createManagerMenu();?>
+     </div>
+  </div>
 </div>
+
+<div class="container-fluid">
+  <div class="row-fluid">
+    <?php 
+    $moduleMenu = commonModel::createModuleMenu($this->moduleName);
+    if($moduleMenu) echo "<div class='span3 leftmenu'>$moduleMenu</div>\n<div class='span9'>\n";
+    ?>
