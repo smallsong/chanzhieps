02445b7e602ac4aa803f417da809f7a9b36f1161
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-27 00:30:39 +0000

    * change common to commonModel.

--- module/common/view/header.html.php
+++ module/common/view/header.html.php
@@ -1,14 +1,13 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <?php include 'header.lite.html.php';?>
-<div class='row' id='topbar'><div class='u-1 a-right'><?php common::printTopBar();?></div></div>
+<div class='row' id='topbar'><div class='u-1 a-right'><?php commonModel::printTopBar();?></div></div>
 <table class='table-1' id='header'>
   <tr valign='middle' class='a-center'>
     <td id='logobox' ><?php $config->features->logo and $app->site->logo ? print("<a href=http://{$app->site->domain}>" . html::image($app->site->fullLogoURL) . '</ a>'): print($app->site->name);?></td>
     <td id='sloganbox'><h1><?php echo $app->site->slogan;?></h1></td>
-    <td id='searchbox'><?php common::printSearch();?></td>
+    <td id='searchbox'><?php commonModel::printSearch();?></td>
   </tr>
 </table>
 
-<div class='row' id='navbar'><?php common::printNavBar()?></div>
-<?php endif;?>
+<div class='row' id='navbar'><?php commonModel::printNavBar()?></div>
 <div id='docbody'>
