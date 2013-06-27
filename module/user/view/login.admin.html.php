3b6ba86ac2e7c043992dc304e4643aa3694d417a
wangchunsheng <chunsheng@cnezsoft.com>
2013-06-27 07:14:15 +0000

    * adjust the position of responser.

--- module/user/view/login.admin.html.php
+++ module/user/view/login.admin.html.php
@@ -1,12 +1,18 @@
 <?php include '../../common/view/header.lite.html.php';?>
 <div class="container">
-  <form method="post" id='ajaxForm' class="radius-5px">
+  <form method="post" id='ajaxForm' class="radius shadow">
     <div id='logo'><?php echo html::image("$themeRoot/default/images/main/logo.login.png");?></div>
+    <div id='responser' class='text-center'></div>
     <?php echo html::input('account','','class="input-block-level"');?>
     <?php echo html::password('password','','class="input-block-level"');?>
-    <?php echo html::checkbox('keepLogin', $lang->user->login->keepLogin);?>
+    <?php // echo html::checkbox('keepLogin', $lang->user->login->keepLogin);?>
     <?php echo html::submitButton($lang->user->login->common, 'btn btn-primary');?>
-    <span id='responser'></span>
   </form>
 </div>
-<?php include '../../common/view/footer.admin.html.php';?>
+<?php
+if($config->debug) js::import($jsRoot . 'jquery/form/min.js');
+if($config->debug) js::import($jsRoot . 'jquery/form/xirang.js');
+if(isset($pageJS)) js::execute($pageJS);
+?>
+</body>
+</html>
