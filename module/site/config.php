4e7dfdb8ed49230f2080f405d963defcc5c992c4
wangchunsheng <chunsheng@cnezsoft.com>
2013-07-02 06:19:45 +0000

    - remove this unused config file.

--- module/site/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$config->site = new stdclass();
-$config->site->create = new stdclass();
-$config->site->create->requiredFields = 'title, domain, code';
-$config->site->edit = new stdclass();
-$config->site->edit->requiredFields   = 'title, domain, code';
