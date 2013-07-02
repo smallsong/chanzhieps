3de654000703bbb05e1a46edd5770c7c9f18cb5f
daitingting <daitingting@xirangit.com>
2013-07-02 01:16:56 +0000

    * rename 模块 to 类目.

--- module/tree/lang/zh-cn.php
+++ module/tree/lang/zh-cn.php
@@ -9,26 +9,26 @@
  * @link        http://www.xirang.biz
  */
 $lang->tree = new stdclass();
-$lang->tree->common      = '模块维护';
+$lang->tree->common      = '类目维护';
 $lang->tree->add         = "添加";
 $lang->tree->edit        = "编辑";
-$lang->tree->addChild    = "添加子模块";
-$lang->tree->delete      = "删除模块";
-$lang->tree->browse      = "模块维护";
-$lang->tree->manage      = "维护模块";
+$lang->tree->addChild    = "添加子类目";
+$lang->tree->delete      = "删除类目";
+$lang->tree->browse      = "类目维护";
+$lang->tree->manage      = "维护类目";
 $lang->tree->fix         = "修复数据";
 $lang->tree->updateOrder = "更新排序";
-$lang->tree->manageChild = "维护子模块";
-$lang->tree->child       = "子模块";
+$lang->tree->manageChild = "维护子类目";
+$lang->tree->child       = "子类目";
 $lang->tree->ajaxGetOptionMenu = "接口:获取下拉列表";
 
-$lang->tree->confirmDelete = "您确定删除该模块吗？";
+$lang->tree->confirmDelete = "您确定删除该类目吗？";
 $lang->tree->successSave   = "成功保存";
 $lang->tree->successFixed  = "成功修复";
 
 $lang->category = new stdclass();
-$lang->category->name    = '模块名称';
-$lang->category->parent  = '上级模块';
+$lang->category->name    = '类目名称';
+$lang->category->parent  = '上级类目';
 $lang->category->desc    = '描述';
 $lang->category->keyword = '关键词';
 $lang->category->owners  = '负责人';
