3ac112c46fa026a85c4e4c9485b8e6efc1215859
daitingting <daitingting@xirangit.com>
2013-07-02 01:22:01 +0000

    * modify the path of treeview.css.

--- module/common/view/treeview.html.php
+++ module/common/view/treeview.html.php
@@ -1,5 +1,5 @@
 <?php if($config->debug):?>
-<link rel='stylesheet' href='<?php echo $siteTheme;?>treeview.css' type='text/css' />
+<link rel='stylesheet' href='<?php echo $themeRoot;?>default/treeview.css' type='text/css' />
 <script src='<?php echo $jsRoot;?>jquery/treeview/min.js' type='text/javascript'></script>
 <?php endif;?>
-<script language='javascript'>$(function() { $(".tree").treeview({ persist: "cookie", collapsed: true, unique: true }) })</script>
+<script language='javascript'>$(function() { $(".tree").treeview({collapsed: true, unique: true }) })</script>
