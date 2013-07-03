2e033e39f08008b95c00938304bef8c0b6a3c625
daitingting <daitingting@xirangit.com>
2013-07-03 02:45:21 +0000

    + add requiredFields.

--- module/article/config.php
+++ module/article/config.php
@@ -3,6 +3,8 @@ if(!isset($config->article)) $config->article = new stdclass();
 $config->article->digest      = 200;
 $config->article->indexCount  = 10;
 $config->article->moduleCount = 10;
+$config->article->create      = new stdClass();
+$config->article->create->requiredFields = 'title, content';
 $config->article->editor      = new stdclass();
 $config->article->editor->create = array('id' => 'content', 'tools' => 'simpleTools');
 $config->article->editor->edit   = array('id' => 'content', 'tools' => 'simpleTools');
