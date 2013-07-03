dd340b2fb31c0055e321a9fd17c129dbca46733c
daitingting <daitingting@xirangit.com>
2013-07-03 08:28:51 +0000

    * separate managechild from browse.

---
+++ module/tree/js/managechild.js
@@ -0,0 +1,4 @@
+$(document).ready(function()
+{
+    $.ajaxForm('#childForm');   
+});
---
+++ module/tree/view/managechild.html.php
@@ -0,0 +1,41 @@
+    <form method='post' id="childForm" action='<?php echo $this->createLink('tree', 'manageChild', "tree=$tree");?>'>
+      <table class='table table-bordered'>
+        <caption><?php echo $lang->tree->manageChild;?></caption>
+        <tr>
+          <td class="w-p10 v-middle" style="vertical-align:middle;">
+            <nobr>
+            <?php
+            $sp = "";
+            foreach($parentCategories as $category)
+            {
+                echo html::a($this->createLink('tree', 'browse', "tree=$tree&categoryID=$category->id"), $sp . $category->name, '', 'class="black"');
+                $sp = '<i class="icon-chevron-right"></i>';
+            }
+            ?>
+            </nobr>
+          </td>
+          <td> 
+            <?php
+            $maxOrder = 0;
+            foreach($sons as $sonCategory)
+            {
+                if($sonCategory->order > $maxOrder) $maxOrder = $sonCategory->order;
+                echo html::input("categories[id$sonCategory->id]", $sonCategory->name, 'style="margin-bottom:5px"') . '<br />';
+            }
+            for($i = 0; $i < TREE::NEW_CHILD_COUNT ; $i ++) echo html::input("categories[]", '', 'style="margin-bottom:5px"') . '<br />';
+           ?>
+          </td>
+        </tr>
+        <tr>
+          <td class='a-center' colspan='2'>
+            <?php 
+            echo html::submitButton() . html::resetButton();
+            echo html::hidden('parentCategoryID', $currentCategoryID);
+            echo html::hidden('maxOrder', $maxOrder);
+            ?>      
+            <input type='hidden' value='<?php echo $currentCategoryID;?>' name='parentCategoryID' />
+          </td>
+        </tr>
+      </table>
+    </form>
+  <?php if(isset($pageJS)) js::execute($pageJS);?>
