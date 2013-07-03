c4cddf0dc144a5a1ad07e55b62a536922be085fe
daitingting <daitingting@xirangit.com>
2013-07-03 08:34:32 +0000

    + add js of load span9.

--- module/tree/js/browse.js
+++ module/tree/js/browse.js
@@ -2,5 +2,6 @@ $(document).ready(function()
 {
     $.ajaxForm('#treeForm');
     $.ajaxLink('#treeForm a[class!=delete]', '.span9');
+    $('.span9').load(createLink('tree', 'managechild', 'tree=article&category='+v.categoryID));
 })
 
