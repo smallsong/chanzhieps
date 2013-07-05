12a6c43539b6d14ced32eba4691c74a63a65bbe7
daitingting <daitingting@xirangit.com>
2013-07-05 05:15:03 +0000

    + rewrite the front index.

--- module/index/view/index.html.php
+++ module/index/view/index.html.php
@@ -1,7 +1,34 @@
 <?php include '../../common/view/header.html.php'; ?>
 <?php include '../../common/view/treeview.html.php'; ?>
-<?php
-$viewFile = $this->session->site->type . '.html.php';
-include $viewFile;
-?>
+<div class="jumbotron radius">
+</div>
+<div class="row-fluid">
+  <div class="span4">
+    <h2><?php echo $lang->index->aboutus;?></h2>
+    <p></p>
+    <p>» 集国内首家PHP开源企业门户系统<br/>
+       » 集品牌宣传、市场营销、产品销售于一体<br/>
+       » SEO，访问量统计<br/>
+       » 永久免费，每月更新</p>
+    <p><a href="#" class="btn">了解更多</a></p>
+  </div>
+  <div class="span4">
+    <h2><?php echo $lang->index->news;?></h2>
+    <p>息壤网络信息技术有限公司网站上线啦！</p>
+    <p>息壤网络信息技术有限公司网站上线啦！</p>
+    <p>息壤网络信息技术有限公司网站上线啦！</p>
+    <p>息壤网络信息技术有限公司网站上线啦！</p>
+    <p><a href="#" class="btn">了解更多</a></p>
+  </div>
+  <div class="span4">
+    <h2><?php echo $lang->index->contact;?></h2>
+    <p> 电   话：0532-8446 2898<br/>
+        地   址：青岛市黄岛区井冈山路东方银座C座1106.<br/>
+    </p>
+    <p><a href="#" class="btn">了解更多</a></p>
+  </div>
+</div>
+<style>
+.jumbotron {height: 400px; border: 1px solid #E4E4E4; margin: 10px 0;text-align: center;}
+</style>
 <?php include '../../common/view/footer.html.php'; ?>
