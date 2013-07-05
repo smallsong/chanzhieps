a45d8bd94cc3f0066e27bd21b8683bf2c59a906b
daitingting <daitingting@xirangit.com>
2013-07-05 05:13:42 +0000

    + add the style of front index.

--- module/common/view/header.html.php
+++ module/common/view/header.html.php
@@ -2,19 +2,31 @@
 <?php include 'header.lite.html.php';?>
 <div class="container">
   <div class="masthead">
-    <h3 class="muted">Project name</h3>
+    <h3 class="muted"><?php echo $config->company->name;?></h3>
     <div class="navbar">
       <div class="navbar-inner">
         <div class="container">
           <ul class="nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Downloads</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
+            <li class="active"><a href="#">首页</a></li>
+            <li><a href="#">关于我们</a></li>
+            <li><a href="#">新闻动态</a></li>
+            <li><a href="#">招聘信息</a></li>
+            <li><a href="#">联系我们</a></li>
           </ul>
         </div>
       </div>
     </div><!-- /.navbar -->
   </div>
+<style>
+body {
+    padding-bottom: 60px;
+    padding-top: 20px;
+}
+.container {margin: 0 auto; max-width: 1000px;}
+.navbar .navbar-inner {padding: 0;}
+.navbar .nav {display: table; margin: 0; width: 100%;}
+.navbar .nav li {display: table-cell; float: none; width: 1%;}
+.navbar .nav li a {border-left: 1px solid rgba(255, 255, 255, 0.75); border-right: 1px solid rgba(0, 0, 0, 0.1); font-weight: bold; text-align: center;}
+.navbar .nav li:first-child a {border-left: 0 none; border-radius: 3px 0 0 3px;}
+.navbar .nav li:last-child a {border-radius: 0 3px 3px 0; border-right: 0 none;}
+</style>
