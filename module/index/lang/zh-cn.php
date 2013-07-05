4a13f49376313d04f00fc35ac0c4b770212522f8
daitingting <daitingting@xirangit.com>
2013-07-05 05:18:51 +0000

    + add the language for index.

--- module/index/lang/zh-cn.php
+++ module/index/lang/zh-cn.php
@@ -8,5 +8,8 @@
  * @version     $Id$
  * @link        http://www.xirang.biz
  */
-$lang->index->common = '首页';
-$lang->index->index  = '首页';
+$lang->index->common  = '首页';
+$lang->index->index   = '首页';
+$lang->index->aboutus ='关于息壤';
+$lang->index->news    ='新闻动态';
+$lang->index->contact ='联系我们';
