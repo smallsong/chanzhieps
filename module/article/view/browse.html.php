e64a3270b5835949476f354746dcb99f9dd360b4
daitingting <daitingting@xirangit.com>
2013-07-06 06:30:17 +0000

    + add the style for article list.

--- module/article/view/browse.html.php
+++ module/article/view/browse.html.php
@@ -1,7 +1,5 @@
 <?php 
 include '../../common/view/header.html.php';
 include '../../common/view/treeview.html.php';
-$common->printPositionBar($module);
-$viewFile = $this->session->site->type . '.html.php';
-include $viewFile;
+include 'portal.html.php';
 include '../../common/view/footer.html.php'; 
--- module/article/view/portal.html.php
+++ module/article/view/portal.html.php
@@ -1,23 +1,16 @@
 <div class='row'>
-  <div class='u-24-19'>
-    <div class='cont bd-none'>
-      <?php $this->block->printBlock($layouts, 'header');?>
-      <table class='table-1 bd-none'>
-        <caption><?php echo $module->name;?></caption>
-        <?php foreach($articles as $article):?>
-        <tr>
-          <td>
-            <?php 
-            if('rssarticle' == $module->tree){ echo html::a($article->link, $article->title, '_blank', "class＝'colorbox'"); }
-            else { echo html::a(inlink('view', "id=$article->id"), $article->title); }
-            ?>
-          </td>
-          <td width='50'><?php echo substr($article->addedDate, 2, 8);?></td>
-        </tr>
-        <?php endforeach;?>
-        <tr><td colspan='2'><?php $pager->show();?></td></tr>
-      </table>
-    </div>
+  <div class='span9'>
+    <?php $this->block->printBlock($layouts, 'header');?>
+    <h2><?php echo $category->name;?></h2>
+    <ul class='article-list'>
+    <?php foreach($articles as $article):?>
+      <li>
+        <h3 class='article-title'><?php echo html::a(inlink('view', "id=$article->id"), $article->title);?></h3>
+        <span><?php echo substr($article->addedDate, 2, 8);?>
+      </li>
+    <?php endforeach;?>
+    </ul>
+    <?php $pager->show();?>
   </div>
-  <div class='u-24-5'><div class='cont pl-10px'><?php include '../../common/view/blockright.html.php';?></div></div>
+  <div class='span3'><?php include '../../common/view/side.html.php';?></div>
 </div>
