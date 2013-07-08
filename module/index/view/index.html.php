d873c1d04bfe0d7ff0065cdb14c4bb38560a13d3
daitingting <daitingting@xirangit.com>
2013-07-08 02:04:15 +0000

    * get the content of index from admin.

--- module/index/view/index.html.php
+++ module/index/view/index.html.php
@@ -1,5 +1,8 @@
 <?php include '../../common/view/header.html.php'; ?>
 <?php include '../../common/view/treeview.html.php'; ?>
+<?php $LatestArticles =  articleModel::getLatestArticle(0,4);
+      $contact        = json_decode($this->config->company->contact);
+?>
 <div class="hero-unit radius">
   <h1>Hello, world!</h1>
   <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
@@ -7,27 +10,23 @@
 <div class="row-fluid">
   <div class="span4">
     <h2><?php echo $lang->index->aboutus;?></h2>
-    <p></p>
-    <p>» 集国内首家PHP开源企业门户系统<br/>
-       » 集品牌宣传、市场营销、产品销售于一体<br/>
-       » SEO，访问量统计<br/>
-       » 永久免费，每月更新</p>
+    <p><?php echo $this->config->company->desc;?></p>
     <p><a href="#" class="btn btn-info">了解更多</a></p>
   </div>
   <div class="span4">
     <h2><?php echo $lang->index->news;?></h2>
-    <p>息壤网络信息技术有限公司网站上线啦！</p>
-    <p>息壤网络信息技术有限公司网站上线啦！</p>
-    <p>息壤网络信息技术有限公司网站上线啦！</p>
-    <p>息壤网络信息技术有限公司网站上线啦！</p>
+    <?php foreach($LatestArticles as $id => $article): ?>
+      <p><?php echo html::a(inlink('view', "id=$id"), $article, '', "class='latest-news'");?></p>
+    <?php endforeach; ?>
     <p><a href="#" class="btn btn-info">了解更多</a></p>
   </div>
   <div class="span4">
     <h2><?php echo $lang->index->contact;?></h2>
-    <p> 电   话：0532-8446 2898<br/>
-        地   址：青岛市黄岛区井冈山路东方银座C座1106.<br/>
-    </p>
-    <p><a href="#" class="btn btn-warning">了解更多</a></p>
+    <?php foreach($contact as $item => $value):?>
+    <?php if($value == "") continue;?>
+    <p><strong><?php echo $lang->company->$item;?>:</strong><?php echo $value;?></p>
+    <?php endforeach;?>
+    <p><a href="#" class="btn btn-info">了解更多</a></p>
   </div>
 </div>
 <?php include '../../common/view/footer.html.php'; ?>
