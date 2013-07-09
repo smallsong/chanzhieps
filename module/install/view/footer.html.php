8dadf62c48a080130d127e5de39ba2ad2fb7ca2b
guanxiying <guanxiying@xirangit.com>
2013-07-09 07:21:45 +0000

    * formated code.

--- module/install/view/footer.html.php
+++ module/install/view/footer.html.php
@@ -1,6 +1,6 @@
 <footer>
   <div class="container">
-  <p class="muted credit a-right"><?php printf($lang->poweredBy, $config->version);?></p>
+    <p class="muted credit a-right"><?php printf($lang->poweredBy, $config->version);?></p>
   </div>
 </footer>
 <?php if(isset($pageJS)) js::execute($pageJS);?>
