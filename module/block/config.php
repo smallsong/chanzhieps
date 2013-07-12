1beea5ef01c44378fe2c646b7094e6872386774a
daitingting <daitingting@xirangit.com>
2013-07-12 03:08:02 +0000

    + Initialize the object.

--- module/block/config.php
+++ module/block/config.php
@@ -1,3 +1,5 @@
 <?php
+$config->block =new stdClass();
+$config->block->editor = new stdClass();
 $config->block->editor->create = array('id' => 'content', 'tools' => 'fullTools');
 $config->block->editor->edit   = array('id' => 'content', 'tools' => 'fullTools');
