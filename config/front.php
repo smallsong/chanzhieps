197f0cafedd4e62de27fd0c63458b420f3ec3250
daitingting <daitingting@xirangit.com>
2013-07-12 09:12:35 +0000

    + add the front groups.

--- config/front.php
+++ config/front.php
@@ -45,7 +45,9 @@ $config->front->groups->user['thread'][]   = 'reply';
 $config->front->groups->user['thread'][]   = 'editthread';
 $config->front->groups->user['thread'][]   = 'editreply';
 $config->front->groups->user['thread'][]   = 'post';
-$config->front->groups->user['thread'][]   = 'post';
+$config->front->groups->user['thread'][]   = 'stick';
+$config->front->groups->user['thread'][]   = 'deletethread';
+$config->front->groups->user['thread'][]   = 'deletereply';
 
 $config->front->groups->user['user'][]     = 'control';
 $config->front->groups->user['user'][]     = 'profile';
