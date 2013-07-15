0b8bca948a72f6324541340ea42779eda3555f17
guanxiying <guanxiying@xirangit.com>
2013-07-15 07:30:58 +0000

    * change forbid use mutual.

---
+++ module/user/js/forbid.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('td.operate a').click(function(){
+    $.getJSON($(this).attr('href'),function(data){
+      bootbox.alert(data.message);
+    });
+    return false;
+  });
+});
