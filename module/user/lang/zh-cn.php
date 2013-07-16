e80852aad805146c1f0ef25745df4631f5ac954d
daitingting <daitingting@xirangit.com>
2013-07-16 11:31:26 +0800

    * change the id.

--- module/user/lang/zh-cn.php
+++ module/user/lang/zh-cn.php
@@ -5,7 +5,7 @@
  * @copyright   Copyright 2013-2013 QingDao XiRang Network Infomation Co,LTD (www.xirang.biz)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     user
- * @version     $Id$
+ * @version     $Id: zh-cn.php 824 2010-05-02 15:32:06Z wwccss $
  * @link        http://www.xirang.biz
  */
 $lang->user->common        = '用户';
