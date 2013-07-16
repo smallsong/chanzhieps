f499fa8deddcf04b6f2962743290fb800e80c120
daitingting <daitingting@xirangit.com>
2013-07-16 11:07:31 +0800

    * change the id.

--- module/user/view/deny.html.php
+++ module/user/view/deny.html.php
@@ -5,7 +5,7 @@
  * @copyright   Copyright 2013-2013 QingDao XiRang Network Infomation Co,LTD (www.xirang.biz)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     XiRangEPS
- * @version     $Id$
+ * @version     $Id: deny.html.php 824 2010-05-02 15:32:06Z wwccss $
  */
 include '../../common/view/header.lite.html.php';
 ?>
