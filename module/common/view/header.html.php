282ead1b7e55d899acb7f84cb0432da7916a19be
guanxiying <guanxiying@xirangit.com>
2013-07-16 00:42:58 +0000

    * modify topbar links style.

--- module/common/view/header.html.php
+++ module/common/view/header.html.php
@@ -4,21 +4,19 @@ include 'header.lite.html.php';
 if(isset($config->site->logo)) $logo = json_decode($this->config->site->logo);
 js::set('lang', $lang->js);
 ?>
-<div class="container">
-  <div id="topbar" class="row">  
-    <div id="topnav-menu" class="pull-right">
-      <?php echo commonModel::printTopBar();?>
-    </div>
-  </div>
+<div class="container" style="padding-top:20px;">
   <div class="masthead">
     <div class="row">
-    <div class="span4"><?php if(isset($logo)):?><img class="logo" src="<?php echo $logo->webPath?>"/><?php endif;?></div>
-       <div class="span8">
-         <div class="slogan">
-           <h3><?php echo $config->company->name;?></h3>
-           <?php echo $this->config->site->slogan;?> 
-         </div>
-       </div>
+      <div class="span3">
+        <?php if(isset($logo)):?><img class="logo" src="<?php echo $logo->webPath?>"/><?php endif;?>
+      </div>
+      <div class="span9">
+        <span id="topnav-menu" class="pull-right"> <?php echo commonModel::printTopBar();?> </span>
+        <div class="slogan">
+          <h3><?php echo $config->company->name;?></h3>
+          <span><?php echo $this->config->site->slogan;?> </span>
+        </div>
+      </div>
     </div>
     <?php echo commonModel::createFrontMenu();?>
   </div>
