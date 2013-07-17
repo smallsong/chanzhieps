05a4d23581c29f00ce2a7ecc8c232603998aa515
daitingting <daitingting@xirangit.com>
2013-07-17 10:45:59 +0800

    * adjust the style for header.

--- module/common/view/header.html.php
+++ module/common/view/header.html.php
@@ -4,14 +4,14 @@ include 'header.lite.html.php';
 if(isset($config->site->logo)) $logo = json_decode($this->config->site->logo);
 js::set('lang', $lang->js);
 ?>
-<div class="container" style="padding-top:20px;">
+<div class="container">
   <div class="masthead">
     <div class="row">
+      <div id="topnav-menu" class="a-right"> <?php echo commonModel::printTopBar();?> </div>
       <div class="span3">
         <?php if(isset($logo)):?><img class="logo" src="<?php echo $logo->webPath?>"/><?php endif;?>
       </div>
-      <div class="span9">
-        <span id="topnav-menu" class="pull-right"> <?php echo commonModel::printTopBar();?> </span>
+      <div class="span9 a-right">
         <div class="slogan">
           <h3><?php echo $config->company->name;?></h3>
           <span><?php echo $this->config->site->slogan;?> </span>
