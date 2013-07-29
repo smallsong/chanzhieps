d761cfe042d2c80715780152de1918eb407df5c7
wwccss <chunsheng@cnezsoft.com>
2013-07-29 14:41:34 +0800

    * adjust code format.

--- module/tree/view/browse.html.php
+++ module/tree/view/browse.html.php
@@ -10,26 +10,25 @@
  */
 ?>
 <?php include '../../common/view/header.admin.html.php';?>
-  <div class="span3">
-    <?php 
-        js::set('categoryID', $currentCategoryID);
-        js::set('tree', $tree);
-    ?>
-    <form method='post' id="treeForm" action='<?php echo $this->createLink('tree', 'updateOrder', "tree=$tree");?>'>
-    <table class='table-1 table-bordered'>
-      <caption><?php echo $title;?></caption>
-      <tr>
-        <td>
-          <div id='main'><?php echo $categories;?></div>
-          <div class='a-center'>
-            <?php echo html::submitButton($lang->tree->updateOrder);?>
-          </div>
-        </td>
-      </tr>
-    </table>
-    </form>
-  </div>
-  <div class="span9">
-  </div>
+<div class="span3">
+  <?php 
+  js::set('categoryID', $currentCategoryID);
+  js::set('tree', $tree);
+  ?>
+  <form method='post' id="treeForm" action='<?php echo $this->createLink('tree', 'updateOrder', "tree=$tree");?>'>
+  <table class='table-1 table-bordered'>
+    <caption><?php echo $title;?></caption>
+    <tr>
+      <td>
+        <div id='main'><?php echo $categories;?></div>
+        <div class='a-center'>
+          <?php echo html::submitButton($lang->tree->updateOrder);?>
+        </div>
+      </td>
+    </tr>
+  </table>
+  </form>
+</div>
+<div class="span9"></div>
 <?php include '../../common/view/treeview.html.php';?>
 <?php include '../../common/view/footer.admin.html.php';?>
