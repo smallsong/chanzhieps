81102b62bb24803df7b75fe3234799e61a5faf48
wwccss <chunsheng@cnezsoft.com>
2013-07-30 15:08:27 +0800

    * open treemenu default.

--- module/common/view/treeview.html.php
+++ module/common/view/treeview.html.php
@@ -1,3 +1,3 @@
 <link rel='stylesheet' href='<?php echo $themeRoot;?>default/treeview.css' type='text/css' />
 <script src='<?php echo $jsRoot;?>jquery/treeview/min.js' type='text/javascript'></script>
-<script language='javascript'>$(function() { $(".tree").treeview({collapsed: true, unique: true }) })</script>
+<script language='javascript'>$(function() {$(".tree").treeview({collapsed: false, unique: false}) })</script>
