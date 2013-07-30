7247194cc8805f47158ae17d1a6f09d3e047bf32
wwccss <chunsheng@cnezsoft.com>
2013-07-30 16:38:44 +0800

    + add hidden field to reply and thread.

--- db/upgrade1.0.sql
+++ db/upgrade1.0.sql
@@ -1,2 +1,6 @@
 -- add primary field of image.
 alter table xr_file add `primary` enum('1','0')  not null default '0';
+
+-- add hidden field to thread and reply.
+ALTER TABLE `xr_thread` ADD `hidden` ENUM( '1', '0' ) NOT NULL DEFAULT '0';
+ALTER TABLE `xr_reply` ADD `hidden` ENUM( '1', '0' ) NOT NULL DEFAULT '0';
