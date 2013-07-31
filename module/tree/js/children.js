bbc4ff5526827e59c2a079dccfcf54e1e447b213
wwccss <chunsheng@cnezsoft.com>
2013-07-31 17:50:07 +0800

    * rewrite the load way of child.

--- module/tree/js/children.js
+++ module/tree/js/children.js
@@ -1,4 +1,10 @@
 $(document).ready(function()
 {
-    $.ajaxForm('#childForm');   
+    $.ajaxForm('#childForm', function()
+    {
+        $('#treeMenuBox').parent().load(createLink('tree', 'browse', 'tree=' + v.treeType) + ' #treeMenuBox', function()
+        {
+            $(".tree").treeview({collapsed: false, unique: false});    
+        });
+    });
 });
