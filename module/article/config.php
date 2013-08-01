0defd4c45ed5f4d81142c12ac3a9aa08383abd49
daitingting <daitingting@xirangit.com>
2013-08-01 14:04:24 +0800

    * add categories in requiredFields.

--- module/article/config.php
+++ module/article/config.php
@@ -5,6 +5,6 @@ $config->article->moduleCount = 10;
 $config->article->create      = new stdClass();
 $config->article->editor      = new stdclass();
 
-$config->article->create->requiredFields = 'title, content';
+$config->article->create->requiredFields = 'categories, title, content';
 $config->article->editor->create = array('id' => 'content', 'tools' => 'simpleTools');
 $config->article->editor->edit   = array('id' => 'content', 'tools' => 'simpleTools');
