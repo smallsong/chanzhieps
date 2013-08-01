36dae2aaeb4957943180b67402d4c2788a6e73a6
guanxiying <guanxiying@xirangit.com>
2013-08-01 14:42:45 +0800

    * fix text-\d+ and select-\d+ width.  * modify form style for article create.

--- module/article/js/common.js
+++ module/article/js/common.js
@@ -1,4 +1,12 @@
 $(document).ready(function()
 {
   $(v.currentMenu).parent('li').addClass('active');
+
+  $('#original').change(function()
+  {
+      $('#copyBox').hide().find(':input').attr('disabled', true);
+      if($(this).val() == 0) $('#copyBox').show().find(':input').attr('disabled', false);
+  });
+
+  $('#original').change();
 });
--- module/article/view/create.html.php
+++ module/article/view/create.html.php
@@ -16,7 +16,7 @@
     <caption><?php echo $lang->article->add;?></caption>
     <tr>
       <th><?php echo $lang->article->category;?></th>
-      <td><?php echo html::select("categories[]", $tree, $category ? $category->id : 0, 'class=select-3');?></td>
+      <td width="90%"><?php echo html::select("categories[]", $tree, $category ? $category->id : 0, 'class=select-3');?></td>
     </tr>
     <tr>
       <th><?php echo $lang->article->author;?></th>
@@ -25,11 +25,13 @@
     <tr>
       <th><?php echo $lang->article->original;?></th>
       <td>
-        <?php
-        echo html::select('original', $lang->article->originalList, 1, 'class=select-3');
-        echo $lang->article->copySite . html::input('copySite', '', 'class=text-2');
-        echo $lang->article->copyURL  . html::input('copyURL', '', 'class=text-3');
-        ?>
+        <?php echo html::select('original', $lang->article->originalList, 1, 'class=select-3');?>
+        <span id="copyBox">
+          <?php
+          echo html::input('copySite', '', "class='text-3' placeholder='{$lang->article->copySite}'");
+          echo html::input('copyURL',  '', "class='text-4' placeholder='{$lang->article->copyURL}'");
+          ?>
+        </span>
       </td>
     </tr>
     <tr>
@@ -58,5 +60,4 @@
     </tr>
   </table>
   </form>
-</div>
 <?php include '../../common/view/footer.admin.html.php';?>
