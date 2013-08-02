77d5209de0a6a87c3ef2297ace524568609ce524
wwccss <chunsheng@cnezsoft.com>
2013-08-02 11:28:33 +0800

    * use relation table instead of articlecategory table.

--- db/upgrade1.0.sql
+++ db/upgrade1.0.sql
@@ -4,3 +4,9 @@ alter table xr_file add `primary` enum('1','0')  not null default '0';
 -- add hidden field to thread and reply.
 ALTER TABLE `xr_thread` ADD `hidden` ENUM( '1', '0' ) NOT NULL DEFAULT '0';
 ALTER TABLE `xr_reply` ADD `hidden` ENUM( '1', '0' ) NOT NULL DEFAULT '0';
+
+ALTER TABLE `xr_articleCategory` CHANGE `article` `id` MEDIUMINT( 9 ) NOT NULL;
+ALTER TABLE `xr_articleCategory` ADD `type` CHAR( 20 ) NOT NULL FIRST ;
+ALTER TABLE `xr_articleCategory` DROP INDEX `article` ,
+ADD UNIQUE `relation` ( `type` , `id` , `category` );
+RENAME TABLE `xirang`.`xr_articleCategory` TO `xirang`.`xr_relation` ;
