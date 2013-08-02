9ffe822ac340f865a2b0f803c088de596d8e9ae0
wwccss <chunsheng@cnezsoft.com>
2013-08-02 11:32:32 +0800

    - rm unused code.  + add edit requiredFields.

--- module/article/config.php
+++ module/article/config.php
@@ -1,10 +1,10 @@
 <?php
-$config->article->digest      = 200;
-$config->article->indexCount  = 10;
-$config->article->moduleCount = 10;
 $config->article->create      = new stdClass();
-$config->article->editor      = new stdclass();
-
 $config->article->create->requiredFields = 'categories, title, content';
+
+$config->article->edit      = new stdClass();
+$config->article->edit->requiredFields = 'categories, title, content';
+
+$config->article->editor      = new stdclass();
 $config->article->editor->create = array('id' => 'content', 'tools' => 'simpleTools');
 $config->article->editor->edit   = array('id' => 'content', 'tools' => 'simpleTools');
