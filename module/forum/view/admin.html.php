33660867452e6af070f63819a4d0b015e855f8d8
guanxiying <guanxiying@xirangit.com>
2013-08-05 15:45:48 +0800

    * adjust style of forum and add delete function backend.

--- module/forum/view/admin.html.php
+++ module/forum/view/admin.html.php
@@ -1,33 +1,39 @@
 <?php include '../../common/view/header.admin.html.php'; ?>
 <table class='table table-hover table-bordered table-striped'>
-  <?php $config->requestType = 'PATH_INFO';?>
   <caption>
     <div class='f-left'><?php echo $lang->forum->threadList;?></div>
   </caption>
   <thead>
     <tr class='a-center'>
-      <th><? echo $lang->thread->title;?></th>
+      <th class='w-id'><?php echo $lang->thread->id;?></th>
+      <th><?php echo $lang->thread->title;?></th>
       <th><?php echo $lang->thread->author;?></th>
       <th><?php echo $lang->thread->postedDate;?></th>
       <th><?php echo $lang->thread->views;?></th>
       <th><?php echo $lang->thread->replies;?></th>
       <th><?php echo $lang->thread->lastReply;?></th>
+      <th class='w-80px a-center'><?php echo $lang->actions;?></th>
     </tr>  
   </thead>
   <tbody>
     <?php foreach($threads as $thread):?>
     <tr class='a-center'>
+      <th><?php echo $thread->id;?></th>
       <td class='a-left'>
         <?php
         $iconRoot = $themeRoot . 'default/images/forum/';
         $thread->isNew ? print(html::image($iconRoot . 'threadnew.gif')) : print(html::image($iconRoot . 'threadcommon.gif'));
         ?>
-        <?php echo html::a($this->createLink('thread', 'view', "id=$thread->id"), $thread->title, '_blank');?></td>
+        <?php echo html::a(commonModel::createFrontLink('thread', 'view', "threadID=$thread->id"), $thread->title, '_blank');?>
+      </td>
       <td class='w-50px'><?php echo $thread->author;?></td>
       <td class='w-100px'><?php echo substr($thread->addedDate, 5, -3);?></td>
       <td class='w-30px'><?php echo $thread->views;?></td>
       <td class='w-30px'><?php echo $thread->replies;?></td>
-      <td class='w-150px'><?php if($thread->replies) echo substr($thread->lastRepliedDate, 5, -3) . ' ' . $thread->lastRepliedBy;?></td>  
+      <td class='w-150px a-left'><?php if($thread->replies) echo substr($thread->lastRepliedDate, 5, -3) . ' ' . $thread->lastRepliedBy;?></td>  
+      <td>
+        <?php echo html::a($this->createLink('thread', 'delete', "threadID=$thread->id"), $lang->delete, '', "class='deleter'"); ?>
+      </td>
     </tr>  
     <?php endforeach;?>
   </tbody>
