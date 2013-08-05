bcbd6be681ff2b5a899370216f80d60ccead691e
wwccss <chunsheng@cnezsoft.com>
2013-08-05 15:21:49 +0800

    * adjust user-thread page.

--- module/user/view/thread.html.php
+++ module/user/view/thread.html.php
@@ -1,10 +1,10 @@
 <?php include '../../common/view/header.html.php';?>
 <div class='row-fluid'>
-<?php include('side.html.php')?>
+  <?php include('side.html.php')?>
   <div class='span9'>
-    <div class='cont'>
-      <table class='table table-hover'>
-      <caption><?php echo $lang->user->thread;?></caption>
+    <table class='table table-bordered table-hover'>
+      <thead>
+        <caption><?php echo $lang->user->thread;?></caption>
         <tr>
           <th><?php echo $lang->thread->title;?></th>
           <th><?php echo $lang->thread->postedDate;?></th>
@@ -12,7 +12,8 @@
           <th><?php echo $lang->thread->replies;?></th>
           <th colspan='2'><?php echo $lang->thread->lastReply;?></th>
         </tr>  
-        <tbody>
+      </thead>
+      <tbody>
         <?php foreach($threads as $thread):?>
         <tr class='a-center'>
           <td class='a-left'><?php echo html::a($this->createLink('thread', 'view', "id=$thread->id"), $thread->title, '_blank');?></td>
@@ -22,10 +23,9 @@
           <td class='w-150px a-left'><?php if($thread->replies) echo substr($thread->lastRepliedDate, 5, -3) . ' ' . $thread->lastRepliedBy;?></td>  
         </tr>  
         <?php endforeach;?>
-        </tbody>
-        <tr><td colspan='8'><?php $pager->show();?></td></tr>
-      </table>
-    </div>
+      </tbody>
+      <tfoot><tr><td colspan='5'><?php $pager->show();?></td></tr></tfoot>
+    </table>
   </div>
 </div>
 <?php include '../../common/view/footer.html.php';?>
