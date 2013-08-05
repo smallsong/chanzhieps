c94c0149ef52aefe040ce07ff48d29570395bcb5
wwccss <chunsheng@cnezsoft.com>
2013-08-05 15:52:08 +0800

    * adjust the admin-index page.

--- module/admin/lang/zh-cn.php
+++ module/admin/lang/zh-cn.php
@@ -1,11 +1,10 @@
 <?php 
-
 $lang->admin->shortcuts = new stdClass();
 
-$lang->admin->shortcuts->browseArticle = '文章列表';
+$lang->admin->shortcuts->adminArticle  = '文章管理';
 $lang->admin->shortcuts->createArticle = '发布文章';
-$lang->admin->shortcuts->category = '维护类目';
-$lang->admin->shortcuts->site = '站点设置';
-$lang->admin->shortcuts->logo = 'LOGO设置';
-$lang->admin->shortcuts->company = '公司信息';
-$lang->admin->shortcuts->contact = '联系方式';
+$lang->admin->shortcuts->category      = '类目管理';
+$lang->admin->shortcuts->site          = '站点设置';
+$lang->admin->shortcuts->logo          = 'LOGO设置';
+$lang->admin->shortcuts->company       = '公司信息';
+$lang->admin->shortcuts->contact       = '联系方式';
--- module/admin/view/index.html.php
+++ module/admin/view/index.html.php
@@ -12,20 +12,20 @@
 <?php include '../../common/view/header.admin.html.php';?>
 <div class="container" id="shortcutContainer">
   <div class="row-fluid">
-    <div class="span5 shortcut article-create"> 
+    <div class="span4 shortcut article-create"> 
       <?php echo html::a($this->createLink('article', 'create'), '<h3>' . $lang->admin->shortcuts->createArticle . '</h3>')?>
     </div>
-    <div class="span3 shortcut article-view"> 
-      <?php echo html::a($this->createLink('article', 'admin'), '<h3>' . $lang->admin->shortcuts->browseArticle . '</h3>')?>
+    <div class="span4 shortcut article-admin"> 
+      <?php echo html::a($this->createLink('article', 'admin'), '<h3>' . $lang->admin->shortcuts->adminArticle . '</h3>')?>
     </div>
     <div class="span4 shortcut category"> 
       <?php echo html::a($this->createLink('tree', 'browse'), '<h3>' . $lang->admin->shortcuts->category . '</h3>')?>
     </div>
     <div class="row-fluid">
-      <div class="span4 shortcut site"> 
+      <div class="span3 shortcut site"> 
         <?php echo html::a($this->createLink('site', 'setBasic'), '<h3>' . $lang->admin->shortcuts->site . '</h3>')?>
       </div>
-      <div class="span2 shortcut logo"> 
+      <div class="span3 shortcut logo"> 
         <?php echo html::a($this->createLink('site', 'setlogo'), '<h3>' . $lang->admin->shortcuts->logo . '</h3>')?>
       </div>
      <div class="span3 shortcut company">
