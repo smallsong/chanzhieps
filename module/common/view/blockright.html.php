6b9116f4143eae25b87ad3ab9e4078fbb853c9d5
wwccss <chunsheng@cnezsoft.com>
2013-08-07 09:11:34 +0800

    - delete them.

--- module/common/view/blockright.html.php
+++
@@ -1,5 +0,0 @@
-<?php if($config->features->tree):?>
-<div class='box-title'><?php echo $lang->articleTree;?></div>
-<div class='box-content'><?php echo $articleTree;?></div>
-<?php endif;?>
-<?php $this->block->printBlock($layouts, 'right');?>
