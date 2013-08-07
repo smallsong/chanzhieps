2231369f3af7dcf32f3dfa196d2721a63ac69e75
wwccss <chunsheng@cnezsoft.com>
2013-08-07 10:38:00 +0800

    * adjust code format.

--- module/article/js/common.js
+++ module/article/js/common.js
@@ -1,12 +1,12 @@
 $(document).ready(function()
 {
-  $(v.currentMenu).parent('li').addClass('active');
-
-  $('#original').change(function()
-  {
-      $('#copyBox').hide().find(':input').attr('disabled', true);
-      if($(this).val() == 0) $('#copyBox').show().find(':input').attr('disabled', false);
-  });
-
-  $('#original').change();
+    $(v.currentMenu).parent('li').addClass('active');
+    
+    /* Set the orginal and copySite, copyURL fields. */
+    $('#original').change(function()
+    {
+        $('#copyBox').hide().find(':input').attr('disabled', true);
+        if($(this).val() == 0) $('#copyBox').show().find(':input').attr('disabled', false);
+    });
+    $('#original').change();
 });
