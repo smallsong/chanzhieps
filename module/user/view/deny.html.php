17224320b51551d54c19772711933ac731721c1c
daitingting <daitingting@xirangit.com>
2013-08-09 15:07:19 +0800

    * adjust the style of deny page.

--- module/user/view/deny.html.php
+++ module/user/view/deny.html.php
@@ -9,26 +9,22 @@
  */
 include '../../common/view/header.lite.html.php';
 ?>
-<div class='row' style='margin-top:100px'>
-  <div class='u-1'>
-  <table align='center' class='table-3 bg-white'> 
-    <caption><?php echo $app->user->account, ' ', $lang->user->deny;?></caption>
-    <tr>
-      <td>
-        <?php
-        $moduleName = isset($lang->$module->common)  ? $lang->$module->common:  $module;
-        $methodName = isset($lang->$module->$method) ? $lang->$module->$method: $method;
+<table class='table table-3'> 
+  <caption><?php echo $app->user->account, ' ', $lang->user->deny;?></caption>
+  <tr>
+    <td>
+      <?php
+      $moduleName = isset($lang->$module->common)  ? $lang->$module->common:  $module;
+      $methodName = isset($lang->$module->$method) ? $lang->$module->$method: $method;
 
-        printf($lang->user->errorDeny, $moduleName, $methodName);
-        echo "<br />";
-        echo html::a($this->createLink($config->default->module), $lang->index->common);
-        if($refererBeforeDeny) echo html::a(helper::safe64Decode($refererBeforeDeny), $lang->user->goback);
-        echo html::a($this->createLink('user', 'logout', "referer=" . helper::safe64Encode($denyPage)), $lang->user->relogin);
-        ?>
-      </td>
-    </tr>  
-  </table>
-  </div>
-</div>  
+      printf($lang->user->errorDeny, $moduleName, $methodName);
+      echo "<br />";
+      echo html::a($this->createLink($config->default->module), $lang->index->common);
+      if($refererBeforeDeny) echo html::a(helper::safe64Decode($refererBeforeDeny), $lang->user->goback);
+      echo html::a($this->createLink('user', 'logout', "referer=" . helper::safe64Encode($denyPage)), $lang->user->relogin);
+      ?>
+    </td>
+  </tr>  
+</table>
 </body>
 </html>
