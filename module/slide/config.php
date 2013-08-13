7730cf53c03baaa04e1964648f8a527eb516c663
guanxiying <guanxiying@xirangit.com>
2013-08-14 07:51:45 +0800

    * Add editor on summary of inquire.

---
+++ module/slide/config.php
@@ -0,0 +1,4 @@
+<?php
+$config->slide->editor = new stdclass();
+$config->slide->editor->create = array('id' => 'summary', 'tools' => 'simpleTools');
+$config->slide->editor->edit   = array('id' => 'summary', 'tools' => 'simpleTools');
