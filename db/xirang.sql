b1264f95e9190cf0a387dc1f954d71bdc4cb0734
guanxiying <guanxiying@xirangit.com>
2013-08-15 09:41:29 +0800

    * new database structor.

--- db/xirang.sql
+++ db/xirang.sql
@@ -1,6 +1,6 @@
 -- DROP TABLE IF EXISTS `xr_article`;
 CREATE TABLE IF NOT EXISTS `xr_article` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(150) NOT NULL,
   `keywords` varchar(150) NOT NULL,
   `summary` text NOT NULL,
@@ -13,60 +13,97 @@ CREATE TABLE IF NOT EXISTS `xr_article` (
   `addedDate` datetime NOT NULL,
   `editedDate` datetime NOT NULL,
   `type` varchar(30) NOT NULL,
-  `views` mediumint(5) unsigned NOT NULL default '0',
-  `sticky` enum('0','1','2','3') NOT NULL default '0',
+  `views` mediumint(5) unsigned NOT NULL DEFAULT '0',
+  `sticky` enum('0','1','2','3') NOT NULL DEFAULT '0',
   `order` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `order` (`order`),
   KEY `views` (`views`),
   KEY `sticky` (`sticky`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `xr_articleCategory`;
-CREATE TABLE IF NOT EXISTS `xr_articleCategory` (
-  `article` mediumint(9) NOT NULL,
-  `category` smallint(5) NOT NULL,
-  UNIQUE KEY `article` (`article`,`category`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_block`;
 CREATE TABLE IF NOT EXISTS `xr_block` (
-  `id` smallint(5) unsigned NOT NULL auto_increment,
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `type` varchar(10) NOT NULL,
   `title` varchar(60) NOT NULL,
   `content` text NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `xr_category`;
+CREATE TABLE IF NOT EXISTS `xr_category` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(30) NOT NULL DEFAULT '',
+  `desc` varchar(150) NOT NULL,
+  `keyword` varchar(150) NOT NULL,
+  `parent` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `path` char(255) NOT NULL DEFAULT '',
+  `grade` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `order` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `type` char(30) NOT NULL,
+  `readonly` enum('0','1') NOT NULL DEFAULT '0',
+  `moderators` varchar(255) NOT NULL,
+  `threads` smallint(5) NOT NULL,
+  `posts` smallint(5) NOT NULL,
+  `postedBy` varchar(30) NOT NULL,
+  `postedDate` datetime NOT NULL,
+  `postID` mediumint(9) NOT NULL,
+  `replyID` mediumint(8) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tree` (`type`),
+  KEY `order` (`order`),
+  KEY `parent` (`parent`),
+  KEY `path` (`path`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_comment`;
 CREATE TABLE IF NOT EXISTS `xr_comment` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `objectType` varchar(30) NOT NULL default '',
-  `objectID` mediumint(8) unsigned NOT NULL default '0',
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `objectType` varchar(30) NOT NULL DEFAULT '',
+  `objectID` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `author` varchar(30) NOT NULL,
   `email` varchar(90) NOT NULL,
   `date` datetime NOT NULL,
   `content` text NOT NULL,
   `ip` varchar(15) NOT NULL,
-  `status` enum('0','1') NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `status` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `status` (`status`),
   KEY `object` (`objectType`,`objectID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `xr_config`;
+CREATE TABLE IF NOT EXISTS `xr_config` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `owner` char(30) NOT NULL DEFAULT '',
+  `module` varchar(30) NOT NULL,
+  `section` char(30) NOT NULL DEFAULT '',
+  `key` char(30) DEFAULT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique` (`owner`,`module`,`section`,`key`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_file`;
 CREATE TABLE IF NOT EXISTS `xr_file` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `pathname` char(50) NOT NULL,
   `title` char(90) NOT NULL,
   `extension` char(30) NOT NULL,
-  `size` mediumint(8) unsigned NOT NULL default '0',
+  `size` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `objectType` char(20) NOT NULL,
   `objectID` mediumint(9) NOT NULL,
-  `addedBy` char(30) NOT NULL default '',
+  `addedBy` char(30) NOT NULL DEFAULT '',
   `addedDate` datetime NOT NULL,
-  `public` enum('1','0') NOT NULL default '1',
-  `downloads` mediumint(8) unsigned NOT NULL default '0',
+  `public` enum('1','0') NOT NULL DEFAULT '1',
+  `downloads` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `extra` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`),
+  `primary` enum('1','0') DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `object` (`objectType`,`objectID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_layout`;
 CREATE TABLE IF NOT EXISTS `xr_layout` (
   `page` varchar(30) NOT NULL,
@@ -74,62 +111,64 @@ CREATE TABLE IF NOT EXISTS `xr_layout` (
   `blocks` varchar(255) NOT NULL,
   UNIQUE KEY `layout` (`page`,`region`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_message`;
 CREATE TABLE IF NOT EXISTS `xr_message` (
-  `id` mediumint(8) NOT NULL auto_increment,
-  `from` char(30) NOT NULL default 'system',
+  `id` mediumint(8) NOT NULL AUTO_INCREMENT,
+  `from` char(30) NOT NULL DEFAULT 'system',
   `to` char(30) NOT NULL,
   `content` varchar(255) NOT NULL,
   `link` varchar(100) NOT NULL,
   `time` datetime NOT NULL,
-  `readed` enum('0','1') NOT NULL default '0',
-  PRIMARY KEY  (`id`),
+  `readed` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `readed` (`readed`),
   KEY `to` (`to`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `xr_category`;
-CREATE TABLE IF NOT EXISTS `xr_category` (
-  `id` smallint(5) unsigned NOT NULL auto_increment,
-  `name` char(30) NOT NULL default '',
-  `desc` varchar(150) NOT NULL,
-  `keyword` varchar(150) NOT NULL,
-  `parent` smallint(5) unsigned NOT NULL default '0',
-  `path` char(255) NOT NULL default '',
-  `grade` tinyint(3) unsigned NOT NULL default '0',
-  `order` smallint(5) unsigned NOT NULL default '0',
-  `tree` char(30) NOT NULL,
-  `readonly` enum('0','1') NOT NULL default '0',
-  `owners` varchar(255) NOT NULL,
-  `threads` smallint(5) NOT NULL,
-  `posts` smallint(5) NOT NULL,
-  `lastPostedBy` varchar(30) NOT NULL,
-  `lastPostedDate` datetime NOT NULL,
-  `lastPostID` mediumint(9) NOT NULL,
-  `lastReplyID` mediumint(8) unsigned NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `tree` (`tree`),
-  KEY `order` (`order`),
-  KEY `parent` (`parent`),
-  KEY `path` (`path`)
+
+-- DROP TABLE IF EXISTS `xr_relation`;
+CREATE TABLE IF NOT EXISTS `xr_relation` (
+  `type` char(20) NOT NULL,
+  `id` mediumint(9) NOT NULL,
+  `category` smallint(5) NOT NULL,
+  UNIQUE KEY `relation` (`type`,`id`,`category`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_reply`;
 CREATE TABLE IF NOT EXISTS `xr_reply` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `thread` mediumint(8) unsigned NOT NULL,
   `content` text NOT NULL,
   `author` char(30) NOT NULL,
   `editor` char(30) NOT NULL,
   `addedDate` datetime NOT NULL,
   `editedDate` datetime NOT NULL,
-  PRIMARY KEY  (`id`),
+  `hidden` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
   KEY `thread` (`thread`),
   KEY `author` (`author`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `xr_site`;
+CREATE TABLE IF NOT EXISTS `xr_site` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `domain` varchar(30) NOT NULL,
+  `code` varchar(30) NOT NULL,
+  `logo` varchar(60) NOT NULL,
+  `slogan` varchar(255) NOT NULL,
+  `mission` text NOT NULL,
+  `theme` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `keywords` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domain` (`domain`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_thread`;
 CREATE TABLE IF NOT EXISTS `xr_thread` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `site` smallint(6) NOT NULL,
-  `category` mediumint(9) NOT NULL,
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `board` mediumint(9) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `author` varchar(60) NOT NULL,
@@ -137,59 +176,49 @@ CREATE TABLE IF NOT EXISTS `xr_thread` (
   `addedDate` datetime NOT NULL,
   `editedDate` datetime NOT NULL,
   `readonly` tinyint(1) NOT NULL,
-  `views` smallint(5) unsigned NOT NULL default '0',
-  `stick` enum('0','1','2','3') NOT NULL default '0',
+  `views` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `stick` enum('0','1','2','3') NOT NULL DEFAULT '0',
   `replies` smallint(6) NOT NULL,
-  `lastRepliedBy` varchar(30) NOT NULL,
-  `lastRepliedDate` datetime NOT NULL,
-  `lastReplyID` mediumint(8) unsigned NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `category` (`category`),
-  KEY `site` (`site`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `repliedBy` varchar(30) NOT NULL,
+  `repliedDate` datetime NOT NULL,
+  `replyID` mediumint(8) unsigned NOT NULL,
+  `hidden` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `category` (`board`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `xr_user`;
 CREATE TABLE IF NOT EXISTS `xr_user` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `account` char(30) NOT NULL default '',
-  `password` char(32) NOT NULL default '',
-  `realname` char(30) NOT NULL default '',
-  `nickname` char(60) NOT NULL default '',
-  `admin` ENUM( 'no', 'common', 'super' ) NOT NULL DEFAULT 'no',
-  `avatar` char(30) NOT NULL default '',
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `account` char(30) NOT NULL DEFAULT '',
+  `password` char(32) NOT NULL DEFAULT '',
+  `realname` char(30) NOT NULL DEFAULT '',
+  `nickname` char(60) NOT NULL DEFAULT '',
+  `admin` enum('no','common','super') NOT NULL DEFAULT 'no',
+  `avatar` char(30) NOT NULL DEFAULT '',
   `birthday` date NOT NULL,
-  `gendar` enum('f','m','u') NOT NULL default 'u',
-  `email` char(90) NOT NULL default '',
+  `gendar` enum('f','m','u') NOT NULL DEFAULT 'u',
+  `email` char(90) NOT NULL DEFAULT '',
   `skype` char(90) NOT NULL,
-  `qq` char(20) NOT NULL default '',
-  `yahoo` char(90) NOT NULL default '',
-  `gtalk` char(90) NOT NULL default '',
-  `wangwang` char(90) NOT NULL default '',
+  `qq` char(20) NOT NULL DEFAULT '',
+  `yahoo` char(90) NOT NULL DEFAULT '',
+  `gtalk` char(90) NOT NULL DEFAULT '',
+  `wangwang` char(90) NOT NULL DEFAULT '',
   `site` varchar(100) NOT NULL,
-  `mobile` char(11) NOT NULL default '',
-  `phone` char(20) NOT NULL default '',
+  `mobile` char(11) NOT NULL DEFAULT '',
+  `phone` char(20) NOT NULL DEFAULT '',
   `company` varchar(255) NOT NULL,
-  `address` char(120) NOT NULL default '',
-  `zipcode` char(10) NOT NULL default '',
-  `visits` mediumint(8) unsigned NOT NULL default '0',
-  `ip` char(15) NOT NULL default '',
+  `address` char(120) NOT NULL DEFAULT '',
+  `zipcode` char(10) NOT NULL DEFAULT '',
+  `visits` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ip` char(15) NOT NULL DEFAULT '',
   `last` datetime NOT NULL,
   `referer` varchar(255) NOT NULL,
-  `addedDate` datetime NOT NULL,
+  `join` datetime NOT NULL,
   `resetKey` char(64) NOT NULL,
   `resetTime` datetime NOT NULL,
   `allowTime` datetime NOT NULL,
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   KEY `admin` (`admin`),
   KEY `account` (`account`,`password`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `xr_config`;
-CREATE TABLE IF NOT EXISTS `xr_config` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `owner` char(30) NOT NULL default '',
-  `module` varchar(30) NOT NULL,
-  `section` char(30) NOT NULL default '',
-  `key` char(30) NOT NULL default '',
-  `value` text NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `unique` (`owner`,`module`,`section`,`key`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
