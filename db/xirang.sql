d1786a543d1b887403a24553f86b28b44693f1e7
guanxiying <guanxiying@xirangit.com>
2013-08-16 10:04:02 +0800

    * change ENGINE of table down.

--- db/xirang.sql
+++ db/xirang.sql
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `xr_down` (
   `referer` varchar(200) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fileID` (`file`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- DROP TABLE IF EXISTS `xr_file`;
 CREATE TABLE IF NOT EXISTS `xr_file` (
