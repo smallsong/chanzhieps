4e1a137e03f437fb5e90b6a1642d34164d835af2
Catouse <catouse@gmail.com>
2013-08-20 22:32:50 +0800

    * rewrite html of navbar for update to bootstrap3.

--- module/common/view/header.html.php
+++ module/common/view/header.html.php
@@ -22,32 +22,28 @@ $topNavs = $this->loadModel('nav')->getNavs('top');
       <div id='slogan' class='col-md-8'><?php echo $this->config->site->slogan;?></div>
       <?php endif;?>
     </div>
-    <div id='topNav' class='navbar navbar-inverse'>
-      <div class='navbar-inner'>
-        <div class='container'>
-          <ul class='nav sf-menu sf-js-enabled grade1'>
-            <?php foreach($topNavs as $nav1):?>
-            <li class="cat-item <?php echo $nav1->class?>"> 
-              <?php echo html::a($nav1->url, $nav1->title);?>
-              <?php if(!empty($nav1->children)):?>
-              <ul class='grade2 children'>
-                <?php foreach($nav1->children as $nav2):?>
-                <li class="cat-item <?php echo $nav2->class?>">
-                  <?php echo html::a($nav2->url, $nav2->title);?>
-                  <?php if(!empty($nav2->children)):?>
-                  <ul class='grade3 children'>
-                    <?php foreach($nav2->children as $nav3):?>
-                    <li class='cat-item'><?php echo html::a($nav3->url, $nav3->title);?></li>
-                    <?php endforeach;?>
-                  </ul>
-                  <?php endif;?>
-                </li>
+    <nav id='topNav' class='navbar' role="navigation">
+      <ul class='nav sf-menu sf-js-enabled grade1'>
+        <?php foreach($topNavs as $nav1):?>
+        <li class="cat-item <?php echo $nav1->class?>"> 
+          <?php echo html::a($nav1->url, $nav1->title);?>
+          <?php if(!empty($nav1->children)):?>
+          <ul class='grade2 children'>
+            <?php foreach($nav1->children as $nav2):?>
+            <li class="cat-item <?php echo $nav2->class?>">
+              <?php echo html::a($nav2->url, $nav2->title);?>
+              <?php if(!empty($nav2->children)):?>
+              <ul class='grade3 children'>
+                <?php foreach($nav2->children as $nav3):?>
+                <li class='cat-item'><?php echo html::a($nav3->url, $nav3->title);?></li>
                 <?php endforeach;?>
               </ul>
               <?php endif;?>
             </li>
             <?php endforeach;?>
           </ul>
-        </div>
-      </div>
-    </div>
+          <?php endif;?>
+        </li>
+        <?php endforeach;?>
+      </ul>
+    </nav>
