5f1efa115b97049bee6b9a465539378f23ace5b8
Catouse <catouse@gmail.com>
2013-08-21 21:19:03 +0800

    * ajust style of admin index page.

--- module/admin/view/index.html.php
+++ module/admin/view/index.html.php
@@ -13,28 +13,42 @@
 <?php include '../../common/view/header.admin.html.php';?>
 <div class='container' id='shortcutBox'>
   <div class='row'>
-    <div class='col-md-4 shortcut article-create'> 
-      <?php echo html::a($this->createLink('article', 'create'), '<h3>' . $lang->admin->shortcuts->createArticle . '</h3>')?>
+    <div class='col-md-4'> 
+      <div class="shortcut article-create">
+        <?php echo html::a($this->createLink('article', 'create'), '<h3>' . $lang->admin->shortcuts->createArticle . '</h3>')?>
+      </div>
     </div>
-    <div class='col-md-4 shortcut article-admin'> 
-      <?php echo html::a($this->createLink('article', 'admin'), '<h3>' . $lang->admin->shortcuts->adminArticle . '</h3>')?>
+    <div class='col-md-4'>
+      <div class="shortcut article-admin">
+        <?php echo html::a($this->createLink('article', 'admin'), '<h3>' . $lang->admin->shortcuts->adminArticle . '</h3>')?>
+      </div>
     </div>
-    <div class='col-md-4 shortcut category'> 
-      <?php echo html::a($this->createLink('tree', 'browse'), '<h3>' . $lang->admin->shortcuts->category . '</h3>')?>
+    <div class='col-md-4'>
+      <div class="shortcut category">
+        <?php echo html::a($this->createLink('tree', 'browse'), '<h3>' . $lang->admin->shortcuts->category . '</h3>')?>  
+      </div>
     </div>
   </div>
   <div class='row'>
-    <div class='col-md-3 shortcut site'> 
-      <?php echo html::a($this->createLink('site', 'setBasic'), '<h3>' . $lang->admin->shortcuts->site . '</h3>')?>
+    <div class='col-md-3'>
+      <div class="shortcut site">
+        <?php echo html::a($this->createLink('site', 'setBasic'), '<h3>' . $lang->admin->shortcuts->site . '</h3>')?>
+      </div>
     </div>
-    <div class='col-md-3 shortcut logo'> 
-      <?php echo html::a($this->createLink('site', 'setlogo'), '<h3>' . $lang->admin->shortcuts->logo . '</h3>')?>
+    <div class='col-md-3'>
+      <div class="shortcut logo">
+        <?php echo html::a($this->createLink('site', 'setlogo'), '<h3>' . $lang->admin->shortcuts->logo . '</h3>')?>
+      </div>
     </div>
-   <div class='col-md-3 shortcut company'>
-      <?php echo html::a($this->createLink('company', 'setBasic'), '<h3>' . $lang->admin->shortcuts->company . '</h3>')?>
+    <div class='col-md-3'>
+      <div class="shortcut company">
+        <?php echo html::a($this->createLink('company', 'setBasic'), '<h3>' . $lang->admin->shortcuts->company . '</h3>')?>
+      </div>
     </div>
-    <div class='col-md-3 shortcut contact'> 
-      <?php echo html::a($this->createLink('company', 'setcontact'), '<h3>' . $lang->admin->shortcuts->contact . '</h3>')?>
+    <div class='col-md-3'>
+      <div class="shortcut contact">
+        <?php echo html::a($this->createLink('company', 'setcontact'), '<h3>' . $lang->admin->shortcuts->contact . '</h3>')?>  
+      </div>
     </div>      
   </div>
 </div>
