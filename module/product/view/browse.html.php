66f799ecb38feb2fa60b860c5e71a5ff3fdd060f
guanxiying <guanxiying@xirangit.com>
2013-08-22 14:07:23 +0800

    * change author in comments.

--- module/product/view/browse.html.php
+++ module/product/view/browse.html.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * The browse view file of product of xirangEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @package     product
+ * @version     $Id$
+ * @link        http://www.xirang.biz
+ */
+?>
 <?php 
 include '../../common/view/header.html.php';
 
--- module/product/view/view.html.php
+++ module/product/view/view.html.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * The create view file of product category of xirangEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @package     product
+ * @version     $Id$
+ * @link        http://www.xirang.biz
+ */
+?>
 <?php 
 include '../../common/view/header.html.php'; 
 include '../../common/view/treeview.html.php'; 
