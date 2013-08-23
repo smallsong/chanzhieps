9b1c2527df3a5227b1ea70e9fb33b7ca04ef02cb
daitingting <daitingting@xirangit.com>
2013-08-23 14:27:00 +0800

    * adjust the code format.

--- module/product/view/view.html.php
+++ module/product/view/view.html.php
@@ -26,51 +26,50 @@ js::set('productID', $product->id);
       
       <div class='content'>
       
-        <div>
-          <?php $title = $product->image->primary->title ? $product->image->primary : $product->title;?>
-          <?php if(empty($product->images)):?>
-          <div class='primary'>
-            <?php  echo html::a(inlink('view', "id=$product->id"), html::image($themeRoot . 'default/images/main/noimage.gif', "title='{$title}' alt='{$product->title}'"), '', "class='big-image'");?>
-          </div>
-          <?php else:?>
-          <div class='primary'>
-            <?php echo html::a(inlink('view', "id=$product->id"), html::image($product->image->primary->smallURL, "title='{$title}' alt='{$product->title}'"), '', "class='big-image'");?>
-            <ul class='list'>
-              <?php foreach($product->image->list as $image):?>
-              <?php $title = $image->title ? $image->title : $product->title;?>
-              <li>
-                <?php echo html::a(inlink('view', "id=$product->id"), html::image($image->smallURL, "title=''"), '', "class='little-image'");?>
-              </li>
-              <?php endforeach;?>
-              <div class='c-both'></div>
-            </ul>
-          </div>
-          <?php endif;?>
+        <?php $title = $product->image->primary->title ? $product->image->primary->title : $product->name;?>
+        <?php if(empty($product->images)):?>
+        <div class='primary'>
+          <?php  echo html::a(inlink('view', "id=$product->id"), html::image($themeRoot . 'default/images/main/noimage.gif', "title='{$title}' alt='{$product->name}'"), '', "class='big-image'");?>
+        </div>
+        <?php else:?>
+        <div class='primary'>
+          <?php echo html::a(inlink('view', "id=$product->id"), html::image($product->image->primary->smallURL, "title='{$title}' alt='{$product->name}'"), '', "class='big-image'");?>
+          <ul class='list'>
+            <?php foreach($product->image->list as $image):?>
+            <?php $title = $image->title ? $image->title : $product->name;?>
+            <li>
+              <?php echo html::a(inlink('view', "id=$product->id"), html::image($image->smallURL, "title='{$title}' alt='{$product->name}'"), '', "class='little-image'");?>
+            </li>
+            <?php endforeach;?>
+            <div class='c-both'></div>
+          </ul>
+        </div>
+        <?php endif;?>
 
-          <div class='property'>
-            <h3><?php echo $product->name;?></h3>
-            <table class='w-p100'>
-              <?php if($product->promotion):?>
-              <tr><th class='w-p50'><?php echo $lang->product->price . $lang->colon;?></th> <td><del><?php echo $lang->RMB . $product->price;?></del></td></tr>
-              <tr><th><?php echo $lang->product->promotion . $lang->colon;?></th> <td><em><?php echo $lang->RMB . $product->promotion;?></em></td></tr>
-              <?php else:?>
-              <tr><th><?php echo $lang->product->price . $lang->colon;?></th> <td><em><?php echo $lang->RMB . $product->price;?></em></td></tr>
-              <?php endif;?>
-              <tr><th><?php echo $lang->product->unit   . $lang->colon;?></th> <td><?php echo $product->unit;?></td></tr>
-              <tr><th><?php echo $lang->product->amount . $lang->colon;?></th> <td><?php echo $product->amount;?></td></tr>
-              <tr><th><?php echo $lang->product->model  . $lang->colon;?></th> <td><?php echo $product->model;?></td></tr>
-              <tr><th><?php echo $lang->product->brand  . $lang->colon;?></th> <td><?php echo $product->brand;?></td></tr>
-              <tr><th><?php echo $lang->product->color  . $lang->colon;?></th> <td><?php echo $product->color;?></td></tr>
-              <tr><th><?php echo $lang->product->origin . $lang->colon;?></th> <td><?php echo $product->origin;?></td></tr>
-            </table>
-          </div>
+        <div class='property'>
+          <h3><?php echo $product->name;?></h3>
+          <table class='w-p100'>
+            <?php if($product->promotion):?>
+            <tr><th class='w-p50'><?php echo $lang->product->price . $lang->colon;?></th> <td><del><?php echo $lang->RMB . $product->price;?></del></td></tr>
+            <tr><th><?php echo $lang->product->promotion . $lang->colon;?></th> <td><em><?php echo $lang->RMB . $product->promotion;?></em></td></tr>
+            <?php else:?>
+            <tr><th><?php echo $lang->product->price . $lang->colon;?></th> <td><em><?php echo $lang->RMB . $product->price;?></em></td></tr>
+            <?php endif;?>
+            <tr><th><?php echo $lang->product->unit   . $lang->colon;?></th> <td><?php echo $product->unit;?></td></tr>
+            <tr><th><?php echo $lang->product->amount . $lang->colon;?></th> <td><?php echo $product->amount;?></td></tr>
+            <tr><th><?php echo $lang->product->model  . $lang->colon;?></th> <td><?php echo $product->model;?></td></tr>
+            <tr><th><?php echo $lang->product->brand  . $lang->colon;?></th> <td><?php echo $product->brand;?></td></tr>
+            <tr><th><?php echo $lang->product->color  . $lang->colon;?></th> <td><?php echo $product->color;?></td></tr>
+            <tr><th><?php echo $lang->product->origin . $lang->colon;?></th> <td><?php echo $product->origin;?></td></tr>
+          </table>
         </div>
         <div class='c-both'></div>
-        <div class=''>
-        <?php echo $product->content;?>
+
+        <div class='description'>
+          <?php echo $product->content;?>
+          <div class='f-left'><?php $this->loadModel('file')->printFiles($product->files);?></div>
+          <div class='c-both'></div>
         </div>
-        <div class='f-left'><?php $this->loadModel('file')->printFiles($product->files);?></div>
-        <div class='c-both'></div>
       </div>
     </div>
     <div id='commentBox'></div>
