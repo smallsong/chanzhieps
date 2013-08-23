b80bb0fa5e0ab6630a9486f998c7e168f9aac918
daitingting <daitingting@xirangit.com>
2013-08-23 11:00:19 +0800

    * adjust the code format.

--- module/product/view/browse.html.php
+++ module/product/view/browse.html.php
@@ -4,7 +4,7 @@
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL
- * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @author      Tingting Dai <daitingting@xirangit.com>
  * @package     product
  * @version     $Id$
  * @link        http://www.xirang.biz
@@ -25,26 +25,26 @@ include '../../common/view/treeview.html.php';
       <h4><?php echo $category->name;?></h4>
       <ul class="media-list">
       <?php foreach($products as $product):?>
-        <li class='media f-left'>
-          <?php 
-          $title = $product->image->primary->title ? $product->image->primary->title : $product->title;
-          if(empty($product->image)) 
-          {
-              echo html::a(inlink('view', "id=$product->id"), html::image($themeRoot . 'default/images/main/noimage.gif', "title='{$title}'"), '', "class='media-image'");
-          }
-          else
-          {
-              echo html::a(inlink('view', "id=$product->id"), html::image($product->image->primary->smallURL, "title='{$title}'"), '', "class='media-image'");
-          }
-          ?>
-          <div class='media-body'>
-            <h5 class='media-heading'><?php echo html::a(inlink('view', "id=$product->id"), $product->name);?></h5>
-            <p>
-              <del><?php echo $lang->RMB . $product->price;?></del>
-              <em><?php echo $lang->RMB . $product->promotion;?></em>
-            </p>
-          </div>
-        </li>
+      <li class='media f-left'>
+        <?php 
+        $title = $product->image->primary->title ? $product->image->primary->title : $product->title;
+        if(empty($product->image)) 
+        {
+            echo html::a(inlink('view', "id=$product->id"), html::image($themeRoot . 'default/images/main/noimage.gif', "title='{$title}'"), '', "class='media-image'");
+        }
+        else
+        {
+            echo html::a(inlink('view', "id=$product->id"), html::image($product->image->primary->smallURL, "title='{$title}'"), '', "class='media-image'");
+        }
+        ?>
+        <div class='media-body'>
+          <h5 class='media-heading'><?php echo html::a(inlink('view', "id=$product->id"), $product->name);?></h5>
+          <p>
+            <del><?php echo $lang->RMB . $product->price;?></del>
+            <em><?php echo $lang->RMB . $product->promotion;?></em>
+          </p>
+        </div>
+      </li>
       <?php endforeach;?>
       <div class='c-both'></div>
       </ul>
