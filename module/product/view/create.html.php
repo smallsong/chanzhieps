2e727eadcefe5b32c2eeefdab0a4c4d19b21fef8
Catouse <catouse@gmail.com>
2013-08-26 22:41:24 +0800

    * remove hover effection of table form when create product.

--- module/product/view/create.html.php
+++ module/product/view/create.html.php
@@ -14,7 +14,7 @@
 <?php include '../../common/view/kindeditor.html.php';?>
 <?php include '../../common/view/chosen.html.php';?>
 <form method='post' class='form-inline' id='ajaxForm'> 
-  <table class='table table-hover table-bordered table-form'>
+  <table class='table table-bordered table-form'>
     <caption><?php echo $lang->product->create;?></caption>
     <tr>
       <th class='w-100px'><?php echo $lang->product->category;?></th>
