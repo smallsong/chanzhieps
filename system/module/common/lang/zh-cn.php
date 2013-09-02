a9e3871065d1c6da886d6ce3aad8eb56dedcb16e
daitingting <daitingting@xirangit.com>
2013-09-02 16:07:02 +0800

    + add translation for help module.

--- system/module/common/lang/zh-cn.php
+++ system/module/common/lang/zh-cn.php
@@ -98,7 +98,7 @@ $lang->menu->user    = '会员|user|admin|';
 $lang->article = new stdclass();
 $lang->article->menu = new stdclass();
 $lang->article->menu->browse = array('link' => '文章列表|article|admin|', 'alias' => 'edit');
-$lang->article->menu->create = '发布文章|article|create|';
+$lang->article->menu->create = '发布文章|article|create|type=article';
 $lang->article->menu->tree   = '类目管理|tree|browse|type=article';
 
 /* Menu of product module. */
@@ -111,7 +111,15 @@ $lang->product->menu->tree   = '类目管理|tree|browse|type=product';
 /* Menu of help module. */
 $lang->help = new stdclass();
 $lang->help->menu = new stdclass();
-$lang->help->menu->browse = array('link' => '教程列表|help|admin|', 'alias' => 'editbook');
+$lang->help->menu->tree       = array('link' => '手册列表|help|admin|', 'alias' => 'editbook');
+$lang->help->menu->createbook = "添加手册|help|createbook|";
+
+/* Menu of help module. */
+$lang->book = new stdclass();
+$lang->book->menu = new stdclass();
+$lang->book->menu->tree          = "目录管理|tree|browse|type=help";
+$lang->book->menu->articleBrowse = "文章管理|article|admin|type=help";
+$lang->book->menu->articleCreate = "发布文章|article|create|type=help";
 
 /* Menu of comment module. */
 $lang->comment = new stdclass();
