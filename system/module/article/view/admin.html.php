ee5d61d7d720172229cfa9c47c546e91409833f0
daitingting <daitingting@xirangit.com>
2013-09-02 15:54:52 +0800

    * add book field.

--- system/module/article/view/admin.html.php
+++ system/module/article/view/admin.html.php
@@ -33,7 +33,7 @@
       <td><?php echo $article->views;?></td>
       <td>
         <?php
-        echo html::a($this->createLink('article', 'edit', "articleID=$article->id"), $lang->edit);
+        echo html::a($this->createLink('article', 'edit', "articleID=$article->id&type=$article->type&book=$article->book"), $lang->edit);
         echo html::a($this->article->createPreviewLink($article->id), $lang->preview, '_blank');
         echo html::a($this->createLink('file',    'browse', "objectType=article&objectID=$article->id"), $lang->article->files, '', "data-toggle='modal' data-width='1000'");
         echo html::a($this->createLink('article', 'delete', "articleID=$article->id"), $lang->delete, '', 'class="deleter"');
