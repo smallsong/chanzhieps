c638ed3f75b49f8e29467701728aec6c3d24d608
daitingting <daitingting@xirangit.com>
2013-09-05 08:48:29 +0800

    * adjust the style of read file.

--- system/module/help/view/read.html.php
+++ system/module/help/view/read.html.php
@@ -1,29 +1,25 @@
 <?php include '../../common/view/header.html.php'; ?>
 <?php include '../../common/view/treeview.html.php'; ?>
 <?php $common->printPositionBar($module, $article);?>
-<div class='row'>
-  <div class='box radius'>
-    <div class='content'>
-      <h1 class='a-center'><?php echo $article->title;?></h1>
-      <div class='a-center mb-10px'>
-        <div class='f-12px'>
-        <?php
-        printf($lang->article->lblAddedDate, $article->addedDate);
-        printf($lang->article->lblAuthor,    $article->author);
-        printf($lang->article->lblViews,     $article->views);
-        ?>
-        </div>
+<div class='box radius'>
+  <div class='content'>
+    <h1 class='a-center'><?php echo $article->title;?></h1>
+    <div class='a-center mb-10px'>
+      <div class='f-12px'>
+      <?php
+      printf($lang->article->lblAddedDate, $article->addedDate);
+      printf($lang->article->lblAuthor,    $article->author);
+      printf($lang->article->lblViews,     $article->views);
+      ?>
       </div>
-      <?php if($article->summary) echo "<div id='summary'><strong>{$lang->article->summary}</strong>$lang->arrow$article->summary</div>";?>
-      <div><?php echo $article->content;;?></div>
-      <div class='a-right'><?php if($article->editor) printf($lang->article->lblEditor, $article->editor, $article->editedDate);?></div>
-      <?php if($article->keywords) echo "<div id='keywords'><strong>{$lang->article->keywords}</strong>$lang->arrow$article->keywords</div>";?>
-      <?php extract($prevAndNext);?>
-      <div class='row f-12px mt-10px'>
-        <div class='col-md-4 a-left'> <?php $prev ? print($lang->article->prev . html::a(inlink('read', "id=$prev[id]"), $prev['title'])) : print($lang->article->none);?></div>
-        <div class='col-md-4 a-center'><?php echo html::a(inlink('book', "book={$book}&category={$category->id}"), $lang->article->directory);?></div>
-        <div class='col-md-4 a-right'><?php $next ? print(html::a(inlink('read', "id=$next[id]"), $next['title']). $lang->article->next) : print($lang->article->none);?></div>
-      </div>
+    </div>
+    <div><?php echo $article->content;;?></div>
+    <?php if($article->keywords) echo "<div id='keywords'><strong>{$lang->article->keywords}</strong>$lang->arrow$article->keywords</div>";?>
+    <?php extract($prevAndNext);?>
+    <div class='row f-12px mt-10px'>
+      <div class='col-md-4 a-left'> <?php $prev ? print($lang->article->prev . html::a(inlink('read', "id=$prev[id]"), $prev['title'])) : print($lang->article->none);?></div>
+      <div class='col-md-4 a-center'><?php echo html::a(inlink('book', "type=$type&category={$category->id}"), $lang->article->directory);?></div>
+      <div class='col-md-4 a-right'><?php $next ? print(html::a(inlink('read', "id=$next[id]"), $next['title']). $lang->article->next) : print($lang->article->none);?></div>
     </div>
   </div>
 </div>
