5eacddd2f73e399e74348388e2aa322b4d555c84
daitingting <daitingting@xirangit.com>
2013-09-06 10:46:50 +0800

    - rm book field.

--- system/module/tree/js/browse.js
+++ system/module/tree/js/browse.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
     /* Load the children of current category when page loaded. */
-    var link = createLink('tree', 'children', 'type=' + v.type + '&book=' + v.book + '&root=' + v.root);
+    var link = createLink('tree', 'children', 'type=' + v.type + '&root=' + v.root);
     $('#categoryBox').load(link);
 
     $.setAjaxLoader('#treeMenuBox .ajax', '#categoryBox');
